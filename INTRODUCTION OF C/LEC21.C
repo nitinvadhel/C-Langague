e1bd66ec01b32dcf79962832ef436a125e20c320
nitinvadhel <121547726+nitinvadhel@users.noreply.github.com>
2022-12-28 15:30:14 +0530

    Add files via upload

---
+++ INTRODUCTION OF C/LEC21.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+
+{
+   clrscr();
+   printf("i am nitin\n");
+   printf("i am 22 years old\n");
+   printf("my school name is sir jj\n");
+   getch();
+}
---
+++ INTRODUCTION OF C/LEC23.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+   clrscr();
+   printf(" - - -\n");
+   printf("|    |\n");
+   printf("N    |\n");
+   printf("I    |\n");
+   printf("T    |\n");
+   printf("I    |\n");
+   printf("N    |\n");
+   printf("|    |\n");
+   printf("= = =");
+   getch();
+}
